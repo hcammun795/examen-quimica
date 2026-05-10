aba71f7b0205d8d704233a90ad18a85de1c5a712
hcammun795 <hcammun795@g.educaand.es>
2026-05-10 18:13:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // CONFIGURACIÓN SUPABASE (Sustituye por tus claves reales)
-const supabaseUrl = 'https://uiletyxxsmsxwjvnfziu.supabase.co/';
+const supabaseUrl = 'https://uiletyxxsmsxwjvnfziu.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpbGV0eXh4c21zeHdqdm5meml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMTM1NzUsImV4cCI6MjA5Mzg4OTU3NX0.d-QGKtt6IQxwfKRw7rEhFF3VGF_x9xIgf_2k4jkSIAk';
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
